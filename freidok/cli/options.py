632338c981a7441016c3fba436757f18890ece60
Alex <alexander.haupt@physiologie.uni-freiburg.de>
2023-02-07 16:37:08 +0100

    Move command line args handling to own module

--- freidok/cli/options.py
+++ freidok/cli/options.py
@@ -1,32 +1,13 @@
 import argparse
-import json
 import os
 import re
-import sys
-import warnings
-from collections.abc import Callable
-from enum import Enum
 from functools import partial
 from pathlib import Path
+from typing import Callable
 
-from dotenv import load_dotenv
+from freidok.utils import str2list
 
-from freidok import modify
-from freidok.client import FreidokApiClient, FreidokFileReader
-from freidok.export import (
-    PublicationsHtmlExporter,
-    PublicationsMarkdownExporter,
-    TemplateExporter,
-    InstitutionsMarkdownExporter,
-    InstitutionsHtmlExporter,
-)
-from freidok.models.institutions import Institutions
-from freidok.models.publications import Publications
-from freidok.utils import str2list, opens
-
-USER_AGENT = "freidok-retrieve/1.0"
-
-PUBLICATION_FIELDS = [
+API_FIELDS_PUBLICATION = [
     "id",
     "link",
     "abstracts",
@@ -93,14 +74,6 @@ publication_fieldsets = {
 }
 
 
-# ExportFormats = Enum('ExportFormats', ['MARKDOWN', 'JSON', 'HTML', 'TEMPLATE'])
-class ExportFormat(str, Enum):
-    MARKDOWN = "markdown"
-    HTML = "html"
-    JSON = "json"
-    TEMPLATE = "template"
-
-
 def env2dict(
     env_prefix: str,
     key_mapper: Callable[[str], str] = str,
@@ -127,8 +100,7 @@ def load_publication_fieldsets():
     publication_fieldsets.update(d)
 
 
-def arguments():
-    load_dotenv()
+def arguments(func_institutions, func_publications):
     load_publication_fieldsets()
 
     intlist = partial(str2list, mapper=int)
@@ -142,7 +114,7 @@ def arguments():
         return x
 
     def year_range_type(y):
-        if m := re.match(r"^\d{4}(?:-\d{4})?$", y):
+        if re.match(r"^\d{4}(?:-\d{4})?$", y):
             return list(map(int, y.split("-")))
         else:
             raise argparse.ArgumentTypeError(f"{y} is not a valid year range")
@@ -164,7 +136,7 @@ def arguments():
     def language_type(value):
         items = str2list(value)
         for item in items:
-            if not (m := re.match(r"^[a-zA-Z]{3}$", item)):
+            if not re.match(r"^[a-zA-Z]{3}$", item):
                 raise argparse.ArgumentTypeError(
                     f"Invalid 3-letter language code: '{item}'"
                 )
@@ -173,7 +145,6 @@ def arguments():
 
     def api_params_type(value):
         params = {}
-        item: str = ""
         for item in value.split():
             try:
                 key, val = item.split("=")
@@ -337,9 +308,9 @@ def arguments():
     group.add_argument(
         "--fields",
         metavar="F[,F...]",
-        type=partial(multi_choice_type, allowed=PUBLICATION_FIELDS),
+        type=partial(multi_choice_type, allowed=API_FIELDS_PUBLICATION),
         help="Field(s) to include in response. Available fields: "
-        + ", ".join(PUBLICATION_FIELDS),
+        + ", ".join(API_FIELDS_PUBLICATION),
     )
 
     group.add_argument(
@@ -384,7 +355,7 @@ def arguments():
         help="Separate individual authors with STR (ignored if --format=json)",
     )
 
-    sub_pub.set_defaults(func=get_publications)
+    sub_pub.set_defaults(func=func_publications)
 
     #
     # subparser: institutions
@@ -413,165 +384,6 @@ def arguments():
         help="Show institutions containing TERM",
     )
 
-    sub_inst.set_defaults(func=get_institutions)
+    sub_inst.set_defaults(func=func_institutions)
 
     return argp_main.parse_args()
-
-
-def run():
-    args = arguments()
-    with warnings.catch_warnings(record=True) as caught_warnings:
-        warnings.simplefilter("always")
-        args.func(args)
-        for warn in caught_warnings:
-            print(f"{warn.category.__name__}: {warn.message}", file=sys.stderr)
-
-
-def get_output_format(args):
-    # template argument overrides any other format argument
-    if args.template:
-        return ExportFormat.TEMPLATE
-
-    if args.format:
-        return args.format
-
-    if args.out:
-        match args.out.suffix.lower():
-            case (".htm" | ".html"):
-                return ExportFormat.HTML
-            case ".json":
-                return ExportFormat.JSON
-            case _:
-                return ExportFormat.MARKDOWN
-
-    return ExportFormat.MARKDOWN
-
-
-def create_freidok_client(args):
-    if args.source.startswith("http"):
-        return FreidokApiClient(
-            base_url=args.source,
-            user_agent=USER_AGENT,
-            dryrun=args.dryrun,
-            default_max_items=args.maxitems,
-        )
-    else:
-        return FreidokFileReader(file=args.source)
-
-
-def get_publications(args):
-    client = create_freidok_client(args)
-
-    # year params
-    if args.years:
-        year_from = args.years[0]
-        year_to = args.years[1] if len(args.years) > 1 else year_from
-    else:
-        year_from = None
-        year_to = None
-
-    # field params
-    if args.fields:
-        fields = args.fields
-    elif args.fieldset:
-        fields = publication_fieldsets[args.fieldset]
-    else:
-        fields = publication_fieldsets["default"]
-
-    # additional API parameters
-    params = {}
-
-    # sort params
-    # todo: make sort parameters available on command line
-    sortfields = []
-    if "publication_year" in fields:
-        sortfields.append("publication_year+desc")
-    if "id" in fields:
-        sortfields.append("id+desc")
-    if sortfields:
-        params["sortfield"] = ",".join(sortfields)
-
-    if args.api_params:
-        params.update(args.api_params)
-
-    data = client.get_publications(
-        ids=args.id,
-        inst_ids=args.inst_id,
-        pers_ids=args.pers_id,
-        proj_ids=args.proj_id,
-        title=args.title,
-        year_from=year_from,
-        year_to=year_to,
-        fields=fields,
-        maxpers=args.maxpers,
-        maxitems=args.maxitems,
-        startitem=args.startitem,
-        **params,
-    )
-
-    if args.langs != ["ALL"]:
-        data = modify.json_strip_languages(data, preferred=args.langs)
-
-    publist = Publications(**data)
-
-    # add pre-formatted authors list to each publication object (_extras_authors)
-    modify.add_author_list_string(
-        publist,
-        abbrev=args.authors_abbrev,
-        reverse=args.authors_reverse,
-        sep=args.authors_sep,
-    )
-
-    if args.authors_abbrev:
-        modify.shorten_author_firstnames(publist, sep=args.authors_abbrev)
-
-    # sort publication links by type
-    modify.sort_links_by_type(publist, preferred=["doi"])
-
-    export(publist, data, args)
-
-
-def get_institutions(args):
-    client = create_freidok_client(args)
-    data = client.get_institutions(
-        ids=args.id,
-        name=args.name,
-    )
-
-    if args.langs != ["ALL"]:
-        data = modify.json_strip_languages(data, preferred=args.langs)
-
-    items = Institutions(**data)
-
-    export(items, data, args)
-
-
-def export(items, data, args):
-    outfile = args.out or "-"
-    outfmt = get_output_format(args)
-    match outfmt:
-        case ExportFormat.HTML if items.type == "publication":
-            PublicationsHtmlExporter.export(items, outfile, template_file=args.template)
-
-        case ExportFormat.HTML if items.type == "institution":
-            InstitutionsHtmlExporter.export(items, outfile, template_file=args.template)
-
-        case ExportFormat.MARKDOWN if items.type == "publication":
-            PublicationsMarkdownExporter.export(
-                items, outfile, template_file=args.template
-            )
-
-        case ExportFormat.MARKDOWN if items.type == "institution":
-            InstitutionsMarkdownExporter.export(
-                items, outfile, template_file=args.template
-            )
-
-        case ExportFormat.TEMPLATE:
-            TemplateExporter().export(items, outfile, template_file=args.template)
-
-        case ExportFormat.JSON:
-            with opens(outfile, encoding="utf-8") as f:
-                json.dump(data, f, indent=2)
-
-        case _:
-            raise NotImplementedError(f"Unsupported format: {outfmt}")
