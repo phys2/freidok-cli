d8f949c44cfee5cb8a3081ced78c6d52cd95132f
Alex <alexander.haupt@physiologie.uni-freiburg.de>
2023-02-15 17:40:59 +0100

    Add code generation script (for development)
    `codegen/generate-models.py`

---
+++ codegen/generate-models.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+"""
+Generate Python code (Pydantic model classes) from JSON Schema.
+"""
+import argparse
+import shlex
+import subprocess
+import sys
+
+FREIDOK_SCHEMA_PUB = (
+    "https://freidok.uni-freiburg.de/site/interfaces?dl=schema_publications"
+)
+
+CMD_NOT_FOUND_MSG = """
+Error: {cmd} cannot be found.
+
+Install with
+     poetry install --with dev
+or
+     pip install datamodel-code-generator[http]
+"""
+
+
+def arguments():
+    argp = argparse.ArgumentParser(
+        prog="generate-models",
+        description=__doc__,
+    )
+
+    argp.add_argument("outfile", help="Output file")
+    argp.add_argument("--schema", metavar="SRC", help="JSON Schema file or URL")
+
+    return argp.parse_args()
+
+
+def main():
+    args = arguments()
+
+    source = args.schema or FREIDOK_SCHEMA_PUB
+
+    if source.startswith(("http:", "https:")):
+        source = f'--url "{source}"'
+    else:
+        source = f'--input "{source}"'
+
+    cmdstr = f"""
+        datamodel-codegen
+        --use-schema-description
+        --use-field-description
+        --target-python-version 3.10
+        --use-double-quotes
+        --use-standard-collections
+        --encoding utf-8
+        --allow-extra-fields
+        --use-annotated
+        --force-optional
+        --output {args.outfile}
+        {source}
+    """
+
+    cmd = shlex.split(cmdstr)
+
+    try:
+        process = subprocess.run(
+            cmd,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            text=True,
+        )
+    except FileNotFoundError:
+        msg = CMD_NOT_FOUND_MSG.format(cmd=cmd[0]).strip()
+        print(msg, file=sys.stderr)
+        exit(1)
+
+    print(process.stdout)
+    if process.returncode:
+        print(f"{cmd[0]} returned an error")
+    exit(process.returncode)
+
+
+if __name__ == "__main__":
+    main()
